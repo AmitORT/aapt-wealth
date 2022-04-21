c6bad0c6a822be263d0db8884e2f17407c353c87
Ashwini More <amit.dhumal@onerooftech.com>
2022-04-21 13:20:11 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,24 @@
 export const environment = {
   production: true,
   ServerUrl:"https://findev.aaptfin.com/api/",
-  CreditUrl:"https://uat.finizon.com/credit/",
-  CommonUrl:" https://uat.finizon.com/",
-  InsuranceUrl:"http://uat.finizoninsurance.com/",
-  WealthUrl:"https://uat.finizon.com/wealth",
+
+
+
+  // --UAT FINIZON--
+
+  //CommonUrl:"https://uat.finizon.com?TOKEN={TOKEN}&PATH={PATH}",
+  //CreditUrl:"https://uat.finizon.com/credit?TOKEN={TOKEN}&PATH={PATH}",  
+  //InsuranceUrl:"http://uat.finizoninsurance.com",
+  //WealthUrl:"https://uat.finizon.com/wealth?TOKEN={TOKEN}&PATH={PATH}",
+
+  // --UAT FINIZON--
+
+  // --UAT ONEROOFTECH--
+
+  CommonUrl:"http://finizon.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
+  CreditUrl:"http://finizon-credit.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
+  InsuranceUrl:"http://finizon-insurance.onerooftechnologies.com/",
+  WealthUrl:"http://finizon-wealth.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
+
+  // --UAT ONEROOFTECH--
 };
