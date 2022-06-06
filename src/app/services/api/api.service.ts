9c5217bc56d20b717d09be3732a0f601caddb8a7
Ashwini More <amit.dhumal@onerooftech.com>
2022-06-06 13:29:44 +0530

    changes

--- src/app/services/api/api.service.ts
+++ src/app/services/api/api.service.ts
@@ -8,54 +8,55 @@ import { AescryptoService } from '../cryptomanager/aescrypto.service';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService { 
+export class ApiService {
   serverUrl = environment.ServerUrl;
-  
-  header = { 
-    headers: new HttpHeaders()
-    .set('Access-Control-Allow-Origin', '*')
-    .set('Access-Control-Allow-Methods', '*')
-    .set('Access-Control-Allow-Headers', 'Content-Type')
-  };
-  
 
-  constructor(private httpClient: HttpClient,private crypto:AescryptoService) { }
-  
-  public get(url: string,auth=false): Observable<any>{
+  header = {
+    headers: new HttpHeaders()
+      .set('Access-Control-Allow-Origin', '*')
+      .set('Access-Control-Allow-Methods', '*')
+      .set('Access-Control-Allow-Headers', 'Content-Type')
+  };
+
+
+  constructor(private httpClient: HttpClient, private crypto: AescryptoService) { }
+
+  public get(url: string, auth = false): Observable<any> {
     url = this.serverUrl + url;
-    if(auth){
-      let token=this.crypto.Decrypt(localStorage.getItem("CustToken")).token;
-      this.header.headers=this.header.headers.set("Authorization","Bearer "+token);
+    if (auth) {
+      let token = this.crypto.Decrypt(localStorage.getItem("CustToken")).token;
+      this.header.headers = this.header.headers.set("Authorization", "Bearer " + token);
     }
-     return this.httpClient.get<any>(url,this.header).pipe(
+    return this.httpClient.get<any>(url, this.header).pipe(
       catchError(this.handleError)
     );
   }
-  
-  public post(url: string, data: any,auth=false,fromdata:boolean=false): Observable<any>{
+
+  public post(url: string, data: any, auth = false, fromdata: boolean = false): Observable<any> {
     url = this.serverUrl + url;
-    if(auth){
-      let token=this.crypto.Decrypt(localStorage.getItem("CustToken")).token;
-      this.header.headers=this.header.headers.set("Authorization","Bearer "+token);
+    if (auth) {
+      let token = this.crypto.Decrypt(localStorage.getItem("CustToken")).token;
+      this.header.headers = this.header.headers.set("Authorization", "Bearer " + token);
     }
-    return this.httpClient.post<any>(url, data,this.header).pipe(
+    return this.httpClient.post<any>(url, data, this.header).pipe(
       catchError(this.handleError)
     );
   }
 
   handleError(error: HttpErrorResponse) {
-    if(error.status == 0){
-      if(localStorage.getItem("SessionServerAlert")==null){
-        localStorage.setItem("SessionServerAlert","1");
+    if (error.status == 0) {
+      if (localStorage.getItem("SessionServerAlert") == null) {
+        localStorage.setItem("SessionServerAlert", "1");
         alert("Server Down!! Please Try Again Later..");
-          window.location.href = "/";
+        window.location.href = "/";
       }
     }
-    else if (error.status!=200) {
-      if(localStorage.getItem("SessionAlert")==null){
-        localStorage.setItem("SessionAlert","1");
+    else if (error.status != 200) {
+      if (localStorage.getItem("SessionAlert") == null) {
+        localStorage.setItem("SessionAlert", "1");
         alert("Session expired..! Please login again");
-          window.location.href = "/";
+        localStorage.clear();
+        window.location.href = "/";
       }
     }
     return throwError('Something bad happened; please try again later.');
