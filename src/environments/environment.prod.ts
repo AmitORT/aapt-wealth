0a86ed93881511f3c0e59ae8ba3c1d1951a64886
Ashwini More <amit.dhumal@onerooftech.com>
2022-06-09 11:44:31 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,21 +4,21 @@ export const environment = {
 
   // --UAT FINIZON--
 
-  // CommonUrl: "https://uat.finizon.com?TOKEN={TOKEN}&PATH={PATH}",
-  // CreditUrl: "https://uat.finizon.com/credit?TOKEN={TOKEN}&PATH={PATH}",
-  // InsuranceUrl: "http://uat.finizoninsurance.com?TOKEN={TOKEN}&PATH={PATH}",
-  // WealthUrl: "https://uat.finizon.com/wealth?TOKEN={TOKEN}&PATH={PATH}",
-  // AgentUrl: "http://finizon-agent.onerooftechnologies.com?ATOKEN={ATOKEN}&PATH={PATH}",
+  CommonUrl: "https://uat.finizon.com?TOKEN={TOKEN}&PATH={PATH}",
+  CreditUrl: "https://uat.finizon.com/credit?TOKEN={TOKEN}&PATH={PATH}",
+  InsuranceUrl: "http://uat.finizoninsurance.com?TOKEN={TOKEN}&PATH={PATH}",
+  WealthUrl: "https://uat.finizon.com/wealth?TOKEN={TOKEN}&PATH={PATH}",
+  AgentUrl: "http://finizon-agent.onerooftechnologies.com?ATOKEN={ATOKEN}&PATH={PATH}",
 
   // --UAT FINIZON--
 
   // --UAT ONEROOFTECH--
 
-  CommonUrl:"http://finizon.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
-  CreditUrl:"http://finizon-credit.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
-  InsuranceUrl:"http://finizon-insurance.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
-  WealthUrl:"http://finizon-wealth.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
-  AgentUrl: "http://finizon-agent.onerooftechnologies.com?ATOKEN={ATOKEN}&PATH={PATH}",
+  // CommonUrl:"http://finizon.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
+  // CreditUrl:"http://finizon-credit.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
+  // InsuranceUrl:"http://finizon-insurance.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
+  // WealthUrl:"http://finizon-wealth.onerooftechnologies.com?TOKEN={TOKEN}&PATH={PATH}",
+  // AgentUrl: "http://finizon-agent.onerooftechnologies.com?ATOKEN={ATOKEN}&PATH={PATH}",
 
   // --UAT ONEROOFTECH--
 };
