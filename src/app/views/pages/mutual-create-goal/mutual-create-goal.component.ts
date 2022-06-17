498118e278d67aedeebbcdd9d8a88784e69c9e42
Ashwini More <amit.dhumal@onerooftech.com>
2022-06-17 18:17:17 +0530

    CHANGES

--- src/app/views/pages/mutual-create-goal/mutual-create-goal.component.ts
+++ src/app/views/pages/mutual-create-goal/mutual-create-goal.component.ts
@@ -6,7 +6,7 @@ import { AescryptoService } from 'src/app/services/cryptomanager/aescrypto.servi
 import { ValidateService } from 'src/app/services/validate/validate.service';
 import { ToastrService } from 'ngx-toastr';
 
-declare var $:any;
+declare var $: any;
 
 
 @Component({
@@ -16,161 +16,176 @@ declare var $:any;
 })
 export class MutualCreateGoalComponent implements OnInit {
 
-  RupeeChange:boolean=false;
-  dateFormat:any;
- 
-  CreateGoal:any={
-    "ImageOfGoal":"",
-    "Savings":"",
-    "Payment_Mode":"",
-    "Goal_Amount":"",
-    "Goal_Duartion":"",
-    "Date_For_Installments":"15",
-    "Return_Rate":"0",
-    "Inflation_Rate":"0",
-    "Saving_Amount":""
+  RupeeChange: boolean = false;
+  dateFormat: any;
+
+  CreateGoal: any = {
+    "ImageOfGoal": "",
+    "Savings": "",
+    "Payment_Mode": "",
+    "Goal_Amount": "",
+    "Goal_Duartion": "",
+    "Date_For_Installments": "15",
+    "Return_Rate": "0",
+    "Inflation_Rate": "0",
+    "Saving_Amount": ""
   }
 
-  Goals:any;
+  Goals: any;
   CreateGoalList: any = []
 
-  DateOfInstallment:any;
-  ProceedCart:any;
-  
-  constructor(public route:Router,private toastr: ToastrService, public validate:ValidateService, private crypto:AescryptoService, private api:ApiService) { }
+  DateOfInstallment: any;
+  ProceedCart: any;
+
+  constructor(public route: Router, private toastr: ToastrService, public validate: ValidateService, private crypto: AescryptoService, private api: ApiService) { }
 
   ngOnInit(): void {
+  }
 
-  } 
-
-  uploadFile($event:any) {
-    var Image:any;
+  uploadFile($event: any) {
+    var Image: any;
     var reader = new FileReader();
     reader.readAsDataURL($event.target.files[0]);
-     Image=$event.target.files[0];
+    Image = $event.target.files[0];
     reader.onload = function () {
-    var ThumbnailBase64 = reader.result;
-    console.log("thumbnail ",ThumbnailBase64);
-    $("#profileimg").attr("src",ThumbnailBase64);
-    // Image=ThumbnailBase64;
+      var ThumbnailBase64 = reader.result;
+      console.log("thumbnail ", ThumbnailBase64);
+      $("#profileimg").attr("src", ThumbnailBase64);
+      // Image=ThumbnailBase64;
     }
     console.log($event.target.files[0]); // outputs the first file
-    console.log("Image",Image)
-    }
+    console.log("Image", Image)
+  }
 
   options12: Options = {
     floor: 0,
     ceil: 20,
-    hidePointerLabels:true,
-    translate: (value12: number, label: any): string => {  
-      switch (label) {  
-          case label.Low:  
-              return "<b>0% Min</b> ₹" + value12; 
-          case label.High:  
-              return "<b>20% Max</b> ₹" + value12;  
-          default:  
-              return value12 + "%" ;  
-      }  
-    }     
+    hidePointerLabels: true,
+    translate: (value12: number, label: any): string => {
+      switch (label) {
+        case label.Low:
+          return "<b>0% Min</b> ₹" + value12;
+        case label.High:
+          return "<b>20% Max</b> ₹" + value12;
+        default:
+          return value12 + "%";
+      }
+    }
   };
 
   options13: Options = {
     floor: 0,
     ceil: 20,
-    hidePointerLabels:true,
-    translate: (value12: number, label: any): string => {  
-      switch (label) {  
-          case label.Low:  
-              return "<b>0% Min</b> ₹" + value12; 
-          case label.High:  
-              return "<b>20% Max</b> ₹" + value12;  
-          default:  
-              return value12 + "%" ;  
-      }  
-    }     
+    hidePointerLabels: true,
+    translate: (value12: number, label: any): string => {
+      switch (label) {
+        case label.Low:
+          return "<b>0% Min</b> ₹" + value12;
+        case label.High:
+          return "<b>20% Max</b> ₹" + value12;
+        default:
+          return value12 + "%";
+      }
+    }
   };
- 
 
-  CreateGoalAndInvest(){
-    
-    if(this.validate.isNullEmptyUndefined(this.CreateGoal.Savings)){
+
+  CreateGoalAndInvest() {
+
+    if (this.validate.isNullEmptyUndefined(this.CreateGoal.Savings)) {
       this.toastr.error('Reason for saving is mandatory');
     }
-    else if(this.validate.isNullEmptyUndefined(this.CreateGoal.Payment_Mode)){
+    else if (this.validate.isNullEmptyUndefined(this.CreateGoal.Payment_Mode)) {
       this.toastr.error('Payment Mode is mandatory');
     }
-    else if(this.validate.isNullEmptyUndefined(this.CreateGoal.Goal_Amount)){
+    else if (this.validate.isNullEmptyUndefined(this.CreateGoal.Goal_Amount)) {
       this.toastr.error('Goal Amount is mandatory');
     }
-    else if(this.validate.isNullEmptyUndefined(this.CreateGoal.Goal_Duartion)){
+    else if (this.validate.isNullEmptyUndefined(this.CreateGoal.Goal_Duartion)) {
       this.toastr.error('Goal Duration is mandatory');
     }
-    else if(this.validate.isNullEmptyUndefined(this.CreateGoal.Date_For_Installments)){
+    else if (this.validate.isNullEmptyUndefined(this.CreateGoal.Date_For_Installments)) {
       this.toastr.error('Date of Month is mandatory');
     }
-    else if(this.CreateGoal.Return_Rate==0){
+    else if (this.CreateGoal.Return_Rate == 0) {
       this.toastr.error('Rate of Return is mandatory');
     }
-    else if(this.CreateGoal.Inflation_Rate==0){
+    else if (this.CreateGoal.Inflation_Rate == 0) {
       this.toastr.error('Rate of Inflation is mandatory');
     }
     else {
-    var postData=new FormData();
-    postData.append("type",this.CreateGoal.Payment_Mode);
-    postData.append("investmentMode",this.CreateGoal.Payment_Mode);
-    postData.append("targetAmount",this.CreateGoal.Goal_Amount);
-    postData.append("startDate",'2021-06-28');
-    postData.append("targetDate",this.CreateGoal.Date_For_Installments);
-    postData.append("rateInflation",this.CreateGoal.Inflation_Rate);
-    postData.append("rateReturn",this.CreateGoal.Return_Rate);
-    postData.append("name",this.CreateGoal.Savings);
+      var postData = new FormData();
+      postData.append("type", this.CreateGoal.Payment_Mode);
+      postData.append("investmentMode", this.CreateGoal.Payment_Mode);
+      postData.append("targetAmount", this.CreateGoal.Goal_Amount);
+      postData.append("startDate", '2021-06-28');
+      postData.append("targetDate", this.CreateGoal.Date_For_Installments);
+      postData.append("rateInflation", this.CreateGoal.Inflation_Rate);
+      postData.append("rateReturn", this.CreateGoal.Return_Rate);
+      postData.append("name", this.CreateGoal.Savings);
 
-    this.api.post("goalDetails/create-goal", postData).subscribe(response=>{
-        
-    if(response.response.n==1){
-      debugger;
-      this.toastr.success(response.response.Msg);
-      this.ProceedToCart();
-      // this.route.navigate(['/mutual-fund-cart']);
-    }
-    else{
-      this.toastr.error(response.response.Msg);
-    }
-  })
+      this.api.post("goalDetails/create-goal", postData).subscribe(response => {
+        console.log('create goal', response)
+        if (response.response.n == 1) {
+          debugger;
+          this.toastr.success(response.response.Msg);
+          this.CreateInvestor(response.goalId);
+          // this.route.navigate(['/mutual-fund-cart']);
+        }
+        else {
+          this.toastr.error(response.response.Msg);
+        }
+      })
 
-      
+
     }
+  }
+
+  CreateInvestor(goalid: any) {
+    var data = { 'panCardNumber': 'IFSPS1505L', 'name': 'Gaurdian1911', 'gender': 1, 'birthDate': '1965-01-01', 'relation': 1 };
+    var postData = new FormData();
+    postData.append("birth_date", "2000-01-01T06:30:00.000Z");
+    postData.append("investor_type", "1");
+    postData.append("pan", "AAXPB4698L");
+    postData.append("date_of_incorporation", "2020-01-01T06:30:00.000Z");
+    postData.append("guardian_details", JSON.stringify(data));
+    this.api.post("wealthfy/add-update-investor-details", postData, true).subscribe(response => {
+      console.log('inverstor create', response)
+      if (response.response.n == 1) {
+        this.ProceedToCart(goalid);
+      }
+    })
   }
 
   // GetOnlyDay(){    
   //   this.CreateGoal.Date_For_Installments=this.CreateGoal.Date_For_Installments.slice(8);
   // }
 
-  ProceedToCart(){
+  ProceedToCart(goalid: any) {
     debugger;
-      var postData = new FormData();
-      postData.append("goalId",'387');
-      postData.append("transactionTypeId",'1');
-      postData.append("endDateForSip",'2023-02-01T11:01:56.652Z');
-      postData.append("instrumentId",'255527');
-      postData.append("totalAmount",'1234');
-      postData.append("modeOfTransaction",'1');
-      postData.append("frequency",'4');
-      postData.append("transactionSubType",'2');
-      postData.append("frequencyDay",'1');
-      postData.append("startDateForSip",'2021-09-01T11:01:56.652Z');
-      postData.append("serviceProviderAccountId",'20753');
-  
-      this.api.post("wealthfy/proceed-to-cart",postData).subscribe(resp=>{
-        this.ProceedCart=resp.data;
-        let encrypted=this.crypto.Encrypt(this.ProceedCart);
-        localStorage.setItem("ProceedCart",encrypted);
-        console.log("ProceedToCart",resp.data)
-  
-        this.route.navigate(["/mutual-fund-cart"])
-        
-      })
-    
-  } 
+    var postData = new FormData();
+    postData.append("goalId", goalid);
+    postData.append("transactionTypeId", '1');
+    postData.append("endDateForSip", '2023-02-01T11:01:56.652Z');
+    postData.append("instrumentId", '255527');
+    postData.append("totalAmount", '1234');
+    postData.append("modeOfTransaction", '1');
+    postData.append("frequency", '4');
+    postData.append("transactionSubType", '2');
+    postData.append("frequencyDay", '1');
+    postData.append("startDateForSip", '2021-09-01T11:01:56.652Z');
+    postData.append("serviceProviderAccountId", '20753');
+
+    this.api.post("wealthfy/proceed-to-cart", postData).subscribe(resp => {
+      this.ProceedCart = resp.data;
+      let encrypted = this.crypto.Encrypt(this.ProceedCart);
+      localStorage.setItem("ProceedCart", encrypted);
+      console.log("ProceedToCart", resp.data)
+
+      this.route.navigate(["/mutual-fund-cart"])
+
+    })
+
+  }
 
 }
--- src/app/views/pages/mutual-select-goal/mutual-select-goal.component.ts
+++ src/app/views/pages/mutual-select-goal/mutual-select-goal.component.ts
@@ -15,115 +15,120 @@ export class MutualSelectGoalComponent implements OnInit {
 
   customOptions: OwlOptions = {
     items: 3,
-		margin: 3,
-		loop:true,
-		stagePadding: 64,
-		responsive:{
-			0:{items:1,stagePadding: 30},
-			480:{items:1,stagePadding: 60},
-			600:{items:2,stagePadding: 30},
-			1000:{items:3},
-			1200:{items:3}
-		},
-		nav: true,
-      // navText: ['Back','Next'],
-      navText: ["<img src='assets/img/arrow_left.svg'>","<img src='assets/img/arrow_right.svg'>"],
-      dots: false,
-      dotsEach: true,
-      lazyLoad: false,
-      autoplay: true,
-      autoplaySpeed: 500,
-      navSpeed: 500,
-      autoplayTimeout: 5000,
-      autoplayHoverPause: true
+    margin: 3,
+    loop: true,
+    stagePadding: 64,
+    responsive: {
+      0: { items: 1, stagePadding: 30 },
+      480: { items: 1, stagePadding: 60 },
+      600: { items: 2, stagePadding: 30 },
+      1000: { items: 3 },
+      1200: { items: 3 }
+    },
+    nav: true,
+    // navText: ['Back','Next'],
+    navText: ["<img src='assets/img/arrow_left.svg'>", "<img src='assets/img/arrow_right.svg'>"],
+    dots: false,
+    dotsEach: true,
+    lazyLoad: false,
+    autoplay: true,
+    autoplaySpeed: 500,
+    navSpeed: 500,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true
   }
 
-  CreateGoalResponse:any;
-  CreateGoalList:any;
-  SelectedGoal:any;
-  InvestWithoutGoalResp:any;
+  CreateGoalResponse: any;
+  CreateGoalList: any;
+  SelectedGoal: any;
+  InvestWithoutGoalResp: any;
 
-  MyGoals:any;
-  Token:any;
-  CreateGoalListToken:any;
-  goals:any;
+  MyGoals: any;
+  Token: any;
+  CreateGoalListToken: any;
+  goals: any;
 
   id: any;
-  
 
-  constructor(public route:Router,public validate:ValidateService, private crypto:AescryptoService, private api:ApiService,private toastr: ToastrService, private router: ActivatedRoute) { }
+
+  constructor(public route: Router, public validate: ValidateService, private crypto: AescryptoService, private api: ApiService, private toastr: ToastrService, private router: ActivatedRoute) { }
 
   ngOnInit(): void {
-    
-    this.GetMyGoals();  
-    
+    this.GetMyGoals();
   }
 
 
-  GetMyGoals(){
-  
-    this.api.get("goalDetails/create-goal",true).subscribe(resp=>{
-      if(resp.response.n==1){        
-        this.MyGoals=resp.data;
+  GetMyGoals() {
+    this.api.get("goalDetails/create-goal", true).subscribe(resp => {
+      if (resp.response.n == 1) {
+        this.MyGoals = resp.data;
         // console.log("my goals",this.MyGoals);
       }
-      else{
-        this.toastr.error(resp.msg)
+      else {
+        this.toastr.error(resp.msg);
       }
-    })    
-  }
-  
-  GetSelectedGoals(index:number){
-    this.MyGoals[index].selectGoal=!this.MyGoals[index].selectGoal;
-    this.SelectedGoal=this.MyGoals.filter((a:any)=>a.selectGoal==1);
-    localStorage.setItem("GetSelectedGoals",this.crypto.Encrypt(this.SelectedGoal));
-    console.log("SelectedGoal",this.SelectedGoal);
-    this.route.navigate(["/mutual-fund-cart"])
-    
+    })
   }
 
-  InvestWithoutGoal(){
-  
-    if(localStorage.getItem("CustToken") == null){
-      console.log("CustToken", localStorage.getItem("CustToken"))
+  GetSelectedGoals(index: number) {
+    this.MyGoals[index].selectGoal = !this.MyGoals[index].selectGoal;
+    this.SelectedGoal = this.MyGoals.filter((a: any) => a.selectGoal == 1);
+    localStorage.setItem("GetSelectedGoals", this.crypto.Encrypt(this.SelectedGoal));
+    console.log("SelectedGoal", this.SelectedGoal);
+    this.route.navigate(["/mutual-fund-cart"]);
+  }
+
+  CreateInvestor() {
+    if (localStorage.getItem("CustToken") == null) {
       this.route.navigate(["/sign-in"])
     }
-    else{
-      var postData=new FormData();
-      postData.append("transactionTypeId","1");
-      postData.append("instrumentId","255527");
-      postData.append("totalAmount","1234");
-      postData.append("modeOfTransaction","1");
-      postData.append("frequency","4");
-      postData.append("transactionSubType","2");
-      postData.append("frequencyDay","1");
-      postData.append("serviceProviderAccountId","20753");
-  
-      this.api.post("wealthfy/proceed-to-cart",postData).subscribe(resp=>{
-        this.InvestWithoutGoalResp=resp.data;
-        let encrypted=this.crypto.Encrypt(this.InvestWithoutGoalResp)
-        localStorage.setItem("InvestWithoutGoal",encrypted)
-        console.log("InvestWithoutGoal",this.InvestWithoutGoalResp)
-        // console.log("InvestWithoutGoalResp",this.InvestWithoutGoalResp)
-        this.route.navigate(["/mutual-fund-cart"])
-       
+    else {
+
+      var data={'panCardNumber': 'IFSPS1505L','name': 'Gaurdian1911','gender': 1,'birthDate': '1965-01-01','relation': 1};
+      var postData = new FormData();
+      postData.append("birth_date", "2000-01-01T06:30:00.000Z");
+      postData.append("investor_type", "1");
+      postData.append("pan", "AAXPB4698L");
+      postData.append("date_of_incorporation", "2020-01-01T06:30:00.000Z");
+      postData.append("guardian_details",JSON.stringify(data));
+      this.api.post("wealthfy/add-update-investor-details", postData, true).subscribe(response => {
+        console.log('inverstor create', response)
+        if(response.response.n==1){
+          this.InvestWithoutGoal();
+        }
       })
     }
-
-
   }
 
-  CreateGoal(){
-    if(localStorage.getItem("CustToken") == null){
-      console.log("CustToken", localStorage.getItem("CustToken"))
+  InvestWithoutGoal() {
+    var postData = new FormData();
+    postData.append("transactionTypeId", "1");
+    postData.append("instrumentId", "255527");
+    postData.append("totalAmount", "1234");
+    postData.append("modeOfTransaction", "1");
+    postData.append("frequency", "4");
+    postData.append("transactionSubType", "2");
+    postData.append("frequencyDay", "1");
+    postData.append("serviceProviderAccountId", "20753");
+    this.api.post("wealthfy/proceed-to-cart", postData).subscribe(resp => {
+      this.InvestWithoutGoalResp = resp.data;
+      let encrypted = this.crypto.Encrypt(this.InvestWithoutGoalResp)
+      localStorage.setItem("InvestWithoutGoal", encrypted)
+      console.log("InvestWithoutGoal", this.InvestWithoutGoalResp)
+      this.route.navigate(["/mutual-fund-cart"])
+    })
+  }
+
+  CreateGoal() {
+    if (localStorage.getItem("CustToken") == null) {
       this.route.navigate(["/sign-in"])
     }
-    else{
+    else {
       this.route.navigate(["/mutual-create-goal"])
     }
   }
 
-  
+
 
 }
 
