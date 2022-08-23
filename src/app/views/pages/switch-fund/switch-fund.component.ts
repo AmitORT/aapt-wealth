17034f173dcfd8ee733ee0541725a2adb1d3a0a3
AshwiniMore9 <ashwini.more@onerooftech.com>
2022-08-23 10:58:11 +0530

    changes

--- src/app/views/pages/switch-fund/switch-fund.component.ts
+++ src/app/views/pages/switch-fund/switch-fund.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-declare var $:any;
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { ApiService } from 'src/app/services/api/api.service';
+import { AescryptoService } from 'src/app/services/cryptomanager/aescrypto.service';
+import { ValidateService } from 'src/app/services/validate/validate.service';
+declare var $: any;
 
 @Component({
   selector: 'app-switch-fund',
@@ -8,57 +13,104 @@ declare var $:any;
 })
 export class SwitchFundComponent implements OnInit {
 
-  DisplayAccordion:boolean=false;
-  Steps:number= 1;
+  DisplayAccordion: boolean = false;
 
-  constructor() { }
+
+  SwitchLoader: boolean = true;
+  SelectedMutualFund: any;
+  MyHoldings: any;
+  MyHoldingsProductOverviewDetails: any;
+  MyGoals: any;
+
+  Switchcart: any = {
+    'instrumentId': '',
+    'goalId': '',
+    'SwitchFromGoal': 0,
+    'myHoldingCurrentValue': '',
+    'myHoldingUnitsOwned': '',
+    'sellType': 'Amount',
+    'totalAmount': '',
+    'quantity': '',
+    'secondaryInstrumentId':'',
+    'secondaryGoalId':'',
+    'serviceProviderAccountId': '',
+    'isAllUnits': '',
+    'fullSwitch':'',
+    'remainingUnits': 0,
+    'modeOfTransaction': 3,
+    "transactionSubType": 1,
+    "transactionTypeId": 11,
+    'isActive': 1
+
+  }
+  SwitchCartItemList: any = [
+    {
+      'isActive': 1
+    },
+    {
+      'isActive': 0
+    }
+  ];
+
+  constructor(public route: Router, private toastr: ToastrService, public validate: ValidateService, private crypto: AescryptoService, private api: ApiService) { }
 
   ngOnInit(): void {
-    $(".applybtn-mob").hide();
-    $(".body-color").scroll(function() {
-      if ($(".body-color").scrollTop() > 150) {
-      $(".applybtn-mob").show();
-      }
-      else {
-      $(".applybtn-mob").hide();
-      }
-      });
 
+    this.GetMyHoldings();
+    this.GetMyGoals();
 
-    $(".body-color").scroll(function () {
-      if($(".body-color").scrollTop() > 150) {
-      $('#sidebar').css('position','fixed');
-      $('#sidebar').css('top','5%');
-      $('#sidebar').css('width',$("#sidebar-main").width()+'px');
-      }
-      
-      else if ($(".body-color").scrollTop() <= 150) {
-      $('#sidebar').css('position','');
-      $('#sidebar').css('top','');
-      $('#sidebar').css('width','');
-      }
-      
-      if ($('#sidebar').offset().top + $("#sidebar").height() > $("#footer").offset().top-100) {
-      $('#sidebar').css('top',-($("#sidebar").offset().top + $("#sidebar").height() - $("#footer").offset().top+100));
-      }
-      });
+    if (localStorage.getItem("SelectedMutualFund") != null) {
+      this.SelectedMutualFund = this.crypto.Decrypt(localStorage.getItem("SelectedMutualFund"))
+      console.log("SelectedMutualFund", this.SelectedMutualFund);
+    }
+
   }
 
-  NextStep(){
-    this.Steps++; 
-  }
-
-  scrolltotop(){
+  scrolltotop() {
     $('.body-color').animate({
       scrollTop: 0
-  }, 0);
+    }, 0);
   }
-  
-  ShowAccordion(){
-    this.DisplayAccordion=true; 
-      $("#collapseOne").collapse('hide');
-      $("#collapseTwo").collapse('show');
+
+  GetMyHoldings() {
+    this.api.get("switchRedeemFunds/myholdings", true).subscribe(response => {
+      if (response.response.n == 1) {
+        this.MyHoldings = response.data;
+        console.log('MyHoldings', this.MyHoldings)
+      }
+    })
   }
+
+  GetMyGoals() {
+    this.api.get("goalDetails/create-goal", true).subscribe(resp => {
+      if (resp.response.n == 1) {
+        this.MyGoals = resp.data;
+        console.log("my goals", this.MyGoals);
+      }
+    })
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
